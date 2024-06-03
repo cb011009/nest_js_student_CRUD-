df90bcf92535abb9b0d8035da4c1ccb446c338bc
Dulanmi Himansa De Silva <cb011009@students.apiit.lk>
2024-06-03 23:24:56 +0530

    Commiting Students & Auth Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsModule } from './student/student.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [],
+  imports: [ 
+
+  MongooseModule.forRoot('mongodb+srv://dulanmihimansa:test123@cluster0.koobcgz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+  StudentsModule,
+  AuthModule,],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
---
+++ src/dtos/create-student.ts
@@ -0,0 +1,16 @@
+import { IsString, IsInt, IsNotEmpty, Min  } from 'class-validator';
+
+export class CreateStudentDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  readonly age: number;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly degree: string;
+}
